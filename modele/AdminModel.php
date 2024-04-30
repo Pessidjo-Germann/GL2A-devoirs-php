24ebdf3d6375b2322cfe9f9151561dfa6010b4a7
germann <germannpessidjo6@gmail.com>
2024-04-30 08:26:27 +0100

    ajout des fichiers

---
+++ modele/AdminModel.php
@@ -0,0 +1,9 @@
+<?php 
+class AdminModel{
+ private idadmin              
+   private iddemande            
+   private pseudo               
+   private password     
+    
+}
+?>
---
+++ readme.md
@@ -0,0 +1,1 @@
+# DEVOIR GL2A
